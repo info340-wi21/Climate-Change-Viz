1e7c387841d39cd889b339e081b095f56ea59f31
Kellen Maier <kmaier7@uw.edu>
2021-02-14 18:48:02 -0800

    Clean data, begin loading in .csv file to be used

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 'use strict';
 
+
+let tempData = d3.csv("data/tempData.csv")
+    .then( function(response) {
+        let dataPromise = response.json();
+        return dataPromise;
+    })
+    .then(function(data) {
+        console.log(data);
+    });
+
+console.log(tempData);
